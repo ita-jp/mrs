7f251fdbd3257140f817b0a4304ec0a99abb3f22
nagaita <tn00gm@gmail.com>
2019-02-16 09:18:20 +0900

    Use eager load for ReservableRoomRepository.

--- mrs/src/main/java/io/nagaita/mrs/domain/repository/ReservableRoomRepository.java
+++ mrs/src/main/java/io/nagaita/mrs/domain/repository/ReservableRoomRepository.java
@@ -4,6 +4,8 @@ import io.nagaita.mrs.domain.model.ReservableRoom;
 import io.nagaita.mrs.domain.model.ReservableRoomId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import javax.persistence.LockModeType;
 import java.time.LocalDate;
@@ -15,6 +17,7 @@ public interface ReservableRoomRepository extends JpaRepository<ReservableRoom, 
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	Optional<ReservableRoom> findOneForUpdateByReservableRoomId(ReservableRoomId reservableRoomId);
 
-	List<ReservableRoom> findByReservableRoomId_reservedDateOrderByReservableRoomId_roomIdAsc(LocalDate reservedDate);
+	@Query("SELECT DISTINCT x FROM ReservableRoom x LEFT JOIN FETCH x.meetingRoom WHERE x.reservableRoomId.reservedDate = :date ORDER BY x.reservableRoomId.roomId ASC")
+	List<ReservableRoom> findByReservableRoomId_reservedDateOrderByReservableRoomId_roomIdAsc(@Param("date") LocalDate reservedDate);
 
 }
